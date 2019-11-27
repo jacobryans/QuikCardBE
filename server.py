e9a9a47f125e4e6a25aa7fcd9173db4145a64bb2
renegade27 <foemaster@live.com>
2019-11-27 16:28:40 -0600

    Push base flask application with email endpoint

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+from dotenv import load_dotenv
+import json
+import subprocess
+import os
+
+app = Flask('email')
+
+@app.route("/email", methods=["POST"])
+def sendEmail():
+    key = os.getenv("SENDGRID_KEY")
+    subprocess.call([
+    'curl',
+    '-X',
+    'POST',
+    "https://api.sendgrid.com/v3/mail/send",
+    "-H",
+    f"Authorization: Bearer {key}",
+    "-H",
+    "Content-Type: application/json",
+    '-d',
+    json.dumps(request.get_json(force=True))
+    ])
+    return ''
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3001)

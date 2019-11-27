2ded5d7d2b12bb5a51dec7552978a197cbe6575a
renegade27 <foemaster@live.com>
2019-11-27 17:08:23 -0600

    Add package to handle CORS

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from dotenv import load_dotenv
 import json
 import subprocess
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/email", methods=["POST"])
 def sendEmail():
